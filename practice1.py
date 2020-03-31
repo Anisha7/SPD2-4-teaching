dd4a49c13550590cbc24df4083df4af620482131
Anisha Jain <anishajain@outlook.com>
2020-03-30 19:40:17 -0700

    Setup and outlined practice set 1

---
+++ practice1.py
@@ -0,0 +1,68 @@
+# Array Problems
+# Using your idea sketches, pseudocode, or whiteboard code 
+# from the brainstorming session, type it in your code editor, 
+# run some test cases, and debug your code so it works correctly. 
+# Implement at least 2 different solutions for each of the 3 
+# problems above (6+ solutions total).
+
+## Problem 1
+# Given an array a of n numbers and a target value t, find two numbers whose sum is t.
+# Example: a=[5, 3, 6, 8, 2, 4, 7], t=10  ⇒  [3, 7] or [6, 4] or [8, 2]
+
+# Simplifications:
+# 1.
+# 2.
+# 3.
+
+# Psuedocode V1:
+
+# Solution V1:
+
+# Psuedocode V2:
+
+# Solution V2:
+
+# Test
+
+
+## Problem 2
+# Given an array a of n numbers and a count k find the k 
+# largest values in the array a.
+# Example: a=[5, 1, 3, 6, 8, 2, 4, 7], k=3  ⇒  [6, 8, 7]
+
+# Simplifications:
+# 1.
+# 2.
+# 3.
+
+# Psuedocode V1:
+
+# Solution V1:
+
+# Psuedocode V2:
+
+# Solution V2:
+
+# Test
+
+
+## Problem 3
+# Given two arrays a and b of numbers and a target value t, 
+# find a number from each array whose sum is closest to t.
+# Example: a=[9, 13, 1, 8, 12, 4, 0, 5],  b=[3, 17, 4, 14, 6],  
+# t=20  ⇒  [13, 6] or [4, 17] or [5, 14]
+
+# Simplifications:
+# 1.
+# 2.
+# 3.
+
+# Psuedocode V1:
+
+# Solution V1:
+
+# Psuedocode V2:
+
+# Solution V2:
+
+# Test
